eb01f347708fe4893bdfffbc5ae3e03dfce04022
Bhagwat dhonde <bhagwatdhonde15@gmail.com>
2025-08-08 14:08:01 +0530

    Add files via upload

---
+++ package sy.java
@@ -0,0 +1,16 @@
+package sy;
+public class arithmetic operators{
+    public static void main(String[]args){
+        int n1=15;
+        int n2=4;
+        int sum=n1+n2;
+        int difference=n1-n2;
+        int multiplication=n1*n2;
+        int division=n1/n2;
+    System.out.println("n1+n2="+sum);
+    System.out.println("n1-n2="+difference);
+    System.out.println("n1*n2="+multiplication);
+    System.out.println("n1/n2="+division);
+
+    }
+}
